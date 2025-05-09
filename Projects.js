706e838dff172195762d8401ac5dc9dddc9d87ef
Mariska Ye <160583907+Mariska7@users.noreply.github.com>
2025-05-09 13:32:31 +0200

    Update Projects.js

--- Projects.js
+++ Projects.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.body.style.opacity = 0;
+    setTimeout(() => {
+        document.body.style.transition = "opacity 1s";
+        document.body.style.opacity = 1;
+    }, 100);
+});
+
+var darkTheme = false;
+
+function toggleTheme() {
+    var body = document.body;
+    var themeIcon = document.getElementById('theme-icon');
+    
+    darkTheme = !darkTheme; 
+    
+    if (darkTheme) {
+        body.classList.add("dark-theme"); 
+        themeIcon.classList.remove("fa-moon");
+        themeIcon.classList.add("fa-sun");
+    } else {
+        body.classList.remove("dark-theme"); 
+        themeIcon.classList.remove("fa-sun");
+        themeIcon.classList.add("fa-moon");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.style.opacity = 0;
     
     setTimeout(() => {
         document.body.style.transition = "opacity 1s";
